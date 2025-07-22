842480264d871bb3021bd9f9841ad14209ed83be
vakadavid <134170125+vakadavid@users.noreply.github.com>
2025-07-23 00:01:22 +0200

    Update customize.sh

--- platform/exynos2100/patches/miscs/customize.sh
+++ platform/exynos2100/patches/miscs/customize.sh
@@ -8,7 +8,6 @@ SET_PROP "vendor" "debug.hwui.skia_atrace_enabled" "false"
 echo "Setting FUSE passthough"
 SET_PROP "vendor" "persist.sys.fuse.passthrough.enable" "true"
 
-<<<<<<< HEAD
 echo "Disabling encryption"
 # Encryption
 LINE=$(sed -n "/^\/dev\/block\/by-name\/userdata/=" "$WORK_DIR/vendor/etc/fstab.exynos2100")
@@ -46,7 +45,7 @@ if [[ "$TARGET_CODENAME" == "r9s" ]]; then
 else
     ADD_TO_WORK_DIR "b0sxxx" "system" "system/apex/com.android.btservices.apex" 0 0 644 "u:object_r:system_file:s0"
 fi 
-=======
+
 # Samsung ODE
 ENTRIES="
 ODE
@@ -56,4 +55,3 @@ keyrefuge
 for e in $ENTRIES; do
     sed -i "/${e}/d" "$WORK_DIR/vendor/etc/fstab.exynos2100"
 done
->>>>>>> parent of d6104bba (platform: squash revert encryption)
