e4cbe0296d0c4ea10fc7c1e1df030ad5b05a2fac
vakadavid <134170125+vakadavid@users.noreply.github.com>
2025-08-03 19:44:10 +0200

    Update customize.sh

--- platform/exynos990/patches/miscs/customize.sh
+++ platform/exynos990/patches/miscs/customize.sh
@@ -39,21 +39,14 @@ echo "Enable Vulkan"
 SET_PROP "vendor" "ro.hwui.use_vulkan" "true"
 SET_PROP "vendor" "debug.hwui.use_hint_manager" "true"
 
-<<<<<<< HEAD
 # Encryption
-LINE="$(sed -n "/^\/dev\/block\/by-name\/userdata/=" "$WORK_DIR/vendor/etc/fstab.exynos990")"
-=======
-echo "Disabling encryption"
-# Replace encryption with fscompress
 LINE=$(sed -n "/^\/dev\/block\/by-name\/userdata/=" "$WORK_DIR/vendor/etc/fstab.exynos990")
-sed -i "${LINE}s/fileencryption=ice/fscompress/g" "$WORK_DIR/vendor/etc/fstab.exynos990"
->>>>>>> second-repo/fifteen
 
 echo "Switching to FBE v2"
 FBE_V1="fileencryption=ice"
 FBE_V2="fscompress,fileencryption=aes-256-xts:aes-256-cts:v2+inlinecrypt_optimized,metadata_encryption=aes-256-xts,keydirectory=/metadata/vold/metadata_encryption"
 sed -i "${LINE}s|resgid=5678|resgid=5678,inlinecrypt|g" "$WORK_DIR/vendor/etc/fstab.exynos990" \
-    && sed -i "${LINE}s|$FBE_V1|$FBE_V2|g" "$WORK_DIR/vendor/etc/fstab.exynos990"
+sed -i "${LINE}s|$FBE_V1|$FBE_V2|g" "$WORK_DIR/vendor/etc/fstab.exynos990"
 
 SET_PROP "vendor" "ro.crypto.allow_encrypt_override" --delete
 SET_PROP "vendor" "ro.crypto.metadata_init_delete_all_keys.enabled" "true"
