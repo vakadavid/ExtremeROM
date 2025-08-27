d6d3d91a8ce4f305cda4c1d3e0544772ac01de00
vakadavid <134170125+vakadavid@users.noreply.github.com>
2025-08-27 14:41:18 +0200

    knjmhzujkl

--- platform/exynos990/patches/miscs/customize.sh
+++ platform/exynos990/patches/miscs/customize.sh
@@ -20,7 +20,7 @@ SET_PROP "vendor" "debug.sf.high_fps_late_sf_phase_offset_ns" "0"
 LOG_STEP_OUT
 
 LOG_STEP_IN "- Setting Adaptive HFR flags"
-if [[ "$TARGET_CODENAME" != "c1s" && "$TARGET_CODENAME" != "c2s" ]]; then
+if [[ "$TARGET_CODENAME" != "c1s" && "$TARGET_CODENAME" != "c2s" && "$TARGET_CODENAME" != "r8s" ]]; then
     SET_PROP "vendor" "debug.sf.show_refresh_rate_overlay_render_rate" "true"
     SET_PROP "vendor" "ro.surface_flinger.game_default_frame_rate_override" "60"
     SET_PROP "vendor" "ro.surface_flinger.use_content_detection_for_refresh_rate" "true"
@@ -28,7 +28,7 @@ if [[ "$TARGET_CODENAME" != "c1s" && "$TARGET_CODENAME" != "c2s" ]]; then
     SET_PROP "vendor" "ro.surface_flinger.set_touch_timer_ms" "300"
     SET_PROP "vendor" "ro.surface_flinger.set_display_power_timer_ms" "200"
     SET_PROP "vendor" "ro.surface_flinger.enable_frame_rate_override" "true"
-elif [[ "$TARGET_CODENAME" == "c1s" ]]; then
+elif [[ "$TARGET_CODENAME" == "c1s" || "$TARGET_CODENAME" == "r8s" ]]; then
     SET_PROP "vendor" "debug.sf.show_refresh_rate_overlay_render_rate" "true"
     SET_PROP "vendor" "ro.surface_flinger.game_default_frame_rate_override" "60"
     SET_PROP "vendor" "ro.surface_flinger.use_content_detection_for_refresh_rate" "false"
@@ -102,7 +102,7 @@ SET_PROP "product" "bluetooth.profile.hid.device.enabled" "true"
 SET_PROP "product" "bluetooth.profile.hid.host.enabled" "true"
 SET_PROP "product" "bluetooth.profile.map.server.enabled" "true"
 SET_PROP "product" "bluetooth.profile.mcp.server.enabled" "false"
-SET_PROP "product" "bluetooth.profile.opp.enabled" "false"
+SET_PROP "product" "bluetooth.profile.opp.enabled" "true"
 SET_PROP "product" "bluetooth.profile.pan.nap.enabled" "true"
 SET_PROP "product" "bluetooth.profile.pan.panu.enabled" "true"
 SET_PROP "product" "bluetooth.profile.pbap.server.enabled" "true"
