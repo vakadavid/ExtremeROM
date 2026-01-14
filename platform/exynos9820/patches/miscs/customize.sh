f606ae316ec08c57375d25ad508ee3148c856049
vakadavid <134170125+vakadavid@users.noreply.github.com>
2026-01-15 00:59:29 +0100

    Revert "platform: squash revert encryption"
    This reverts commit d6104bba5b1a52024504ce8c9cf011e3fc949892.
    
    # Conflicts:
    #	platform/exynos2100/patches/miscs/customize.sh
    #	platform/exynos9820/patches/miscs/customize.sh
    #	platform/exynos990/patches/miscs/customize.sh

--- platform/exynos9820/patches/miscs/customize.sh
+++ platform/exynos9820/patches/miscs/customize.sh
@@ -1,11 +1,4 @@
-LOG "- Disabling encryption"
-LINE=$(sed -n "/^\/dev\/block\/by-name\/userdata/=" "$WORK_DIR/vendor/etc/fstab.exynos9820")
-sed -i "${LINE}s/,fileencryption=ice//g" "$WORK_DIR/vendor/etc/fstab.exynos9820"
-
-# ODE
-sed -i -e "/ODE/d" -e "/keydata/d" -e "/keyrefuge/d" "$WORK_DIR/vendor/etc/fstab.exynos9820"
-
-LOG_STEP_IN "- Enabling updateable APEX images"
+echo "Enabling updateable APEX images"
 SET_PROP "vendor" "ro.apex.updatable" "true"
 LOG_STEP_OUT
 
@@ -44,6 +37,44 @@ LOG_STEP_IN "- Enabling Vulkan"
 SET_PROP "vendor" "ro.hwui.use_vulkan" "true"
 SET_PROP "vendor" "debug.hwui.use_hint_manager" "true"
 LOG_STEP_OUT
+
+# Encryption
+LINE="$(sed -n "/^\/dev\/block\/by-name\/userdata/=" "$WORK_DIR/vendor/etc/fstab.exynos9820")"
+
+echo "Setting /data to F2FS"
+OLD_FLAGS="noatime,nosuid,nodev,noauto_da_alloc,discard,journal_checksum,data=ordered,errors=panic"
+NEW_FLAGS="noatime,nosuid,nodev,discard,usrquota,grpquota,fsync_mode=nobarrier,reserve_root=32768,resgid=5678"
+sed -i "${LINE}s|ext4|f2fs|g" "$WORK_DIR/vendor/etc/fstab.exynos9820" \
+    && sed -i "${LINE}s|$OLD_FLAGS|$NEW_FLAGS|g" "$WORK_DIR/vendor/etc/fstab.exynos9820"
+
+echo "Switching to FBE v2"
+FBE_V1="fileencryption=ice"
+FBE_V2="fscompress,fileencryption=aes-256-xts:aes-256-cts:v2+inlinecrypt_optimized,metadata_encryption=aes-256-xts,keydirectory=/metadata/vold/metadata_encryption"
+sed -i "${LINE}s|resgid=5678|resgid=5678,inlinecrypt|g" "$WORK_DIR/vendor/etc/fstab.exynos9820" \
+    && sed -i "${LINE}s|$FBE_V1|$FBE_V2|g" "$WORK_DIR/vendor/etc/fstab.exynos9820"
+
+SET_PROP "vendor" "ro.crypto.allow_encrypt_override" --delete
+SET_PROP "vendor" "ro.crypto.metadata_init_delete_all_keys.enabled" "true"
+SET_PROP "vendor" "ro.crypto.dm_default_key.options_format.version" "2"
+SET_PROP "vendor" "ro.crypto.volume.metadata.method" "dm-default-key"
+SET_PROP "vendor" "ro.crypto.volume.options" "::v2"
+
+# Samsung ODE
+ENTRIES="
+ODE
+keydata
+keyrefuge
+"
+for e in $ENTRIES; do
+    sed -i "/${e}/d" "$WORK_DIR/vendor/etc/fstab.exynos9820"
+done
+
+# Add /metadata
+echo "Adding /metadata in /vendor/etc/fstab.exynos9820"
+MISC="/dev/block/by-name/misc\t/misc\temmc\tdefaults\tdefaults,first_stage_mount"
+METADATA="/dev/block/by-name/metadata\t/metadata\text4\tnoatime,nosuid,nodev,noauto_da_alloc,discard,journal_checksum,data=ordered,errors=panic,sync\twait,first_stage_mount,formattable,check"
+
+sed -i "\|${MISC}|a ${METADATA}" "$WORK_DIR/vendor/etc/fstab.exynos9820"
 
 # For some reason we are missing 2 permissions here: android.hardware.security.model.compatible and android.software.controls
 # First one is related to encryption and second one to SmartThings Device Control
