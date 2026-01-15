cb27e74c599c21e1a8d5075ff8762cd7de820505
vakadavid <134170125+vakadavid@users.noreply.github.com>
2026-01-15 21:22:09 +0100

    Update customize.sh

--- platform/exynos990/patches/camera/customize.sh
+++ platform/exynos990/patches/camera/customize.sh
@@ -4,7 +4,7 @@ system/lib64/libenn_wrapper_system.so
 system/lib64/libpic_best.arcsoft.so
 system/lib64/libarcsoft_dualcam_portraitlighting.so
 system/lib64/libdualcam_refocus_gallery_54.so
-system/lib64/libdualcam_refocus_gallery_50.so
+system/lib64/libdualcam_refocus_gallery_48.so
 system/lib64/libhybrid_high_dynamic_range.arcsoft.so
 system/lib64/libae_bracket_hdr.arcsoft.so
 system/lib64/libface_recognition.arcsoft.so
