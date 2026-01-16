be3f7a9eca9d402112c6fe50f4e8ecbba6ed8fc4
vakadavid <134170125+vakadavid@users.noreply.github.com>
2026-01-16 21:15:49 +0100

    Update download_fw.sh

--- scripts/download_fw.sh
+++ scripts/download_fw.sh
@@ -174,7 +174,7 @@ for i in "${FIRMWARES[@]}"; do
         (
         cd "$OUT_DIR"
         STR=""
-        [ $MODEL == "SM-S731B" ] && STR=" -v S731BXXU1AYH9/S731BOXM1AYH9/S731BXXU1AYH9/S731BXXU1AYH9"
+        [ $MODEL == "SM-S731B" ] && STR=" -v S721BXXS7BYH1/S721BOXM7BYH1/S721BXXS7BYH1/S721BXXS7BYH1"
         samloader -m "$MODEL" -r "$CSC" -i "$IMEI" -s "$SERIAL_NO" download$STR -O "$ODIN_DIR/${MODEL}_${CSC}" || exit 1
         )
 
