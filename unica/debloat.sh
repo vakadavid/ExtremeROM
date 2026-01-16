5d51c147ece888d0514a289d87de72468c3312f5
vakadavid <134170125+vakadavid@users.noreply.github.com>
2026-01-16 13:08:40 +0100

    Update debloat.sh

--- unica/debloat.sh
+++ unica/debloat.sh
@@ -175,6 +175,7 @@ if $SOURCE_IS_ESIM_SUPPORTED; then
         SYSTEM_DEBLOAT+="
         system/etc/permissions/privapp-permissions-com.samsung.android.app.esimkeystring.xml
         system/etc/permissions/privapp-permissions-com.samsung.euicc.mep.xml
+        system/etc/permissions/privapp-permissions-com.samsung.euicc.xml
         system/etc/sysconfig/preinstalled-packages-com.samsung.android.app.esimkeystring.xml
         system/etc/sysconfig/preinstalled-packages-com.samsung.euicc.xml
         system/priv-app/EsimKeyString
