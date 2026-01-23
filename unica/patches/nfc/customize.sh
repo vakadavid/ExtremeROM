c185855fab55517313fa59c9dece497a86e30071
vakadavid <134170125+vakadavid@users.noreply.github.com>
2026-01-23 17:26:16 +0100

    Update customize.sh

--- unica/patches/nfc/customize.sh
+++ unica/patches/nfc/customize.sh
@@ -1,10 +1,25 @@
-if [[ "$TARGET_NFC_CHIP_VENDOR" == "NXP" && "$SOURCE_NFC_CHIP_VENDOR" == "SLSI" ]]; then
+if [[ "$TARGET_NFC_CHIP_VENDOR" == "SLSI" && "$SOURCE_NFC_CHIP_VENDOR" == "NXP" ]]; then
+    BLOBS_LIST="
+    system/lib64/libnfc_nci_jni.so
+    system/lib64/libnfc_prop_extn.so
+    system/lib64/libnfc_vendor_extn.so
+    "
+    for blob in $BLOBS_LIST
+    do
+        DELETE_FROM_WORK_DIR "system" "$blob"
+    done
+
     BLOBS_LIST="
     system/lib64/libnfc_sec_jni.so
-    system/lib64/libnfc-nci_flags.so
-    system/lib64/libnfc-sec.so
-    system/lib64/libstatslog_nfc.so
-    system/priv-app/NfcNci/lib/arm64/libnfc_sec_jni.so
+    system/etc/libnfc-nci.conf
+    "
+    for blob in $BLOBS_LIST
+    do
+        ADD_TO_WORK_DIR "e2sxxx" "system" "$blob" 0 0 644 "u:object_r:system_lib_file:s0"
+    done
+elif [[ "$TARGET_NFC_CHIP_VENDOR" == "NXP" && "$SOURCE_NFC_CHIP_VENDOR" == "SLSI" ]]; then
+    BLOBS_LIST="
+    system/lib64/libnfc_sec_jni.so
     "
     for blob in $BLOBS_LIST
     do
@@ -13,17 +28,14 @@ if [[ "$TARGET_NFC_CHIP_VENDOR" == "NXP" && "$SOURCE_NFC_CHIP_VENDOR" == "SLSI" 
 
     BLOBS_LIST="
     system/etc/libnfc-nci.conf
-    system/lib64/libnfc_nxpsn_jni.so
-    system/lib64/vendor.samsung.hardware.nfc_aidl-V1-ndk.so
-    system/lib64/vendor.samsung.hardware.nfc@2.0.so
+    system/lib64/libnfc_nci_jni.so
+    system/lib64/libnfc_prop_extn.so
+    system/lib64/libnfc_vendor_extn.so
     "
     for blob in $BLOBS_LIST
     do
-        ADD_TO_WORK_DIR "pa3qzcx" "system" "$blob" 0 0 644 "u:object_r:system_lib_file:s0"
+        ADD_TO_WORK_DIR "q7qxxx" "system" "$blob" 0 0 644 "u:object_r:system_lib_file:s0"
     done
-
-    ln -sf "/system/lib64/libnfc_nxpsn_jni.so" "$WORK_DIR/system/system/priv-app/NfcNci/lib/arm64/libnfc_nxpsn_jni.so"
-    SET_METADATA "system" "system/priv-app/NfcNci/lib/arm64/libnfc_nxpsn_jni.so" 0 0 644 "u:object_r:system_file:s0"
 else
-    LOG "- NFC chip is not NXP. Ignoring."
+    LOG "- Target NFC chip matches source chip. Ignoring."
 fi
