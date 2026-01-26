a0662c9f72b22baf91570173cf776a35e329766b
vakadavid <134170125+vakadavid@users.noreply.github.com>
2026-01-26 13:55:29 +0100

    Update debloat.sh

--- unica/debloat.sh
+++ unica/debloat.sh
@@ -28,134 +28,69 @@ VENDOR_DEBLOAT+="
 etc/dpolicy
 "
 
-# Samsung PROCA certificate DB & uwb
+# Samsung SIM Unlock
 SYSTEM_DEBLOAT+="
-
-system/etc/proca.db
-system/etc/init/digitalkey_init_uwb_tss2.rc
-system/etc/init/sdp_cryptod.rc
+system/bin/ssud
+system/etc/init/ssu_r13sxxx.rc
+system/etc/init/ssu.rc
+system/etc/permissions/privapp-permissions-com.samsung.ssu.xml
+system/etc/sysconfig/samsungsimunlock.xml
+system/lib64/android.security.securekeygeneration-ndk.so
+system/lib64/vendor.samsung.hardware.security.ssu-V1-ndk.so
+system/lib64/libssu_keystore2.so
+system/priv-app/SsuService
 "
+
 # Recovery restoration script
 VENDOR_DEBLOAT+="
-recovery-from-boot.p
-bin/install-recovery.sh
 etc/init/vendor_flash_recovery.rc
 "
 
 # Apps debloat
 PRODUCT_DEBLOAT+="
-app/AssistantShell
-app/BardShell
-app/Chrome
-app/DuoStub
+app/Chrome64
+app/Duo
 app/Gmail2
 app/Maps
 app/YouTube
 overlay/GmsConfigOverlaySearchSelector.apk
 priv-app/SearchSelector
-priv-app/Messages
 "
 
-# PDP apps
 SYSTEM_DEBLOAT+="
-system/preload
-"
-
-# HwModuleTest
-SYSTEM_DEBLOAT+="
-system/app/Cameralyzer
-system/app/FactoryAirCommandManager
-system/app/FactoryCameraFB
-system/app/HMT
-system/app/WlanTest
-system/etc/default-permissions/default-permissions-com.sec.factory.cameralyzer.xml
-system/etc/permissions/privapp-permissions-com.samsung.android.providers.factory.xml
-system/etc/permissions/privapp-permissions-com.sec.facatfunction.xml
-system/priv-app/FacAtFunction
-system/priv-app/FactoryTestProvider
-"
-
-# Live Transcribe
-SYSTEM_DEBLOAT+="
-system/app/LiveTranscribe
-system/etc/sysconfig/feature-a11y-preload.xml
-"
-
-# Samsung Language Core
-SYSTEM_DEBLOAT+="
-system/etc/permissions/signature-permissions-com.samsung.android.offline.languagemodel.xml
-system/priv-app/OfflineLanguageModel_stub
-"
-
-# Samsung Reminder
-SYSTEM_DEBLOAT+="
-system/app/SmartReminder
-"
-
-# SettingsHelper
-SYSTEM_DEBLOAT+="
-system/etc/permissions/privapp-permissions-com.samsung.android.settingshelper.xml
-system/etc/sysconfig/settingshelper.xml
-system/priv-app/SHClient
-"
-
-# Smart Touch Call
-SYSTEM_DEBLOAT+="
-system/etc/default-permissions/default-permissions-com.samsung.android.visualars.xml
-system/etc/permissions/privapp-permissions-com.samsung.android.visualars.xml
-system/priv-app/SmartTouchCall
-"
-
-# SVC Agent
-SYSTEM_DEBLOAT+="
-system/etc/permissions/privapp-permissions-com.samsung.android.svcagent.xml
-system/priv-app/SVCAgent
-"
-
-# SVoiceIME
-SYSTEM_DEBLOAT+="
-system/priv-app/SVoiceIME
-"
-# UWB
-SYSTEM_DEBLOAT+="
-system/etc/permissions/privapp-permissions-com.sec.android.app.uwbtest.xml
-system/etc/libuwb-cal.conf
-system/framework/semuwb-service.jar
-system/lib/libtflite_uwb_jni.so
-"
-
-
-SYSTEM_DEBLOAT+="
-system/app/ARCore
-system/app/BixbyWakeup
-system/app/BBCAgent
 system/app/CarrierDefaultApp
 system/app/ccinfo
 system/app/ChromeCustomizations
-system/app/ClockPackage
-system/app/DsmsAPK
 system/app/Fast
 system/app/FBAppManager_NS
 system/app/KidsHome_Installer
 system/app/MAPSAgent
 system/app/MDMApp
-system/app/MinusOnePage
-system/app/MoccaMobile
 system/app/PlayAutoInstallConfig
 system/app/Rampart
 system/app/SamsungPassAutofill_v1
+system/app/SamsungTTSVoice_ar_AE_m00
+system/app/SamsungTTSVoice_de_DE_f00
+system/app/SamsungTTSVoice_en_GB_f00
+system/app/SamsungTTSVoice_es_ES_f00
+system/app/SamsungTTSVoice_es_MX_f00
+system/app/SamsungTTSVoice_es_US_f00
+system/app/SamsungTTSVoice_fr_FR_f00
+system/app/SamsungTTSVoice_hi_IN_f00
+system/app/SamsungTTSVoice_id_ID_f00
+system/app/SamsungTTSVoice_it_IT_f00
+system/app/SamsungTTSVoice_pl_PL_f00
+system/app/SamsungTTSVoice_pt_BR_f00
+system/app/SamsungTTSVoice_ru_RU_f00
+system/app/SamsungTTSVoice_th_TH_f00
+system/app/SamsungTTSVoice_vi_VN_f00
 system/app/SilentLog
-system/app/SamsungCalendar
 system/app/SimAppDialog
 system/app/Traceur
 system/app/UniversalMDMClient
 system/app/WifiGuider
-system/app/AirGlance
-system/app/Foundation
-system/app/Roboto
-system/app/SamsungOne
-system/hidden/INTERNAL_SDCARD
 system/etc/default-permissions/default-permissions-com.sec.spp.push.xml
+system/etc/init/digitalkey_init_ble_tss2.rc
 system/etc/init/samsung_pass_authenticator_service.rc
 system/etc/permissions/authfw.xml
 system/etc/permissions/com.samsung.feature.ipsgeofence.xml
@@ -164,7 +99,6 @@ system/etc/permissions/org.carconnectivity.android.digitalkey.rangingintent.xml
 system/etc/permissions/org.carconnectivity.android.digitalkey.secureelement.xml
 system/etc/permissions/privapp-permissions-com.microsoft.skydrive.xml
 system/etc/permissions/privapp-permissions-com.samsung.android.app.updatecenter.xml
-system/etc/permissions/privapp-permissions-com.samsung.android.game.gamehome.xml
 system/etc/permissions/privapp-permissions-com.samsung.android.authfw.xml
 system/etc/permissions/privapp-permissions-com.samsung.android.carkey.xml
 system/etc/permissions/privapp-permissions-com.samsung.android.dkey.xml
@@ -173,7 +107,6 @@ system/etc/permissions/privapp-permissions-com.samsung.android.samsungpass.xml
 system/etc/permissions/privapp-permissions-com.samsung.android.samsungpositioning.xml
 system/etc/permissions/privapp-permissions-com.samsung.android.spayfw.xml
 system/etc/permissions/privapp-permissions-com.samsung.oda.service.xml
-system/etc/permissions/privapp-permissions-com.samsung.android.dqagent.xml
 system/etc/permissions/privapp-permissions-com.sec.android.diagmonagent.xml
 system/etc/permissions/privapp-permissions-com.sec.android.soagent.xml
 system/etc/permissions/privapp-permissions-com.sec.bcservice.xml
@@ -191,25 +124,22 @@ system/etc/sysconfig/samsungauthframework.xml
 system/etc/sysconfig/samsungpassapp.xml
 system/etc/sysconfig/samsungpushservice.xml
 system/hidden/SmartTutor
-system/priv-app/AirCommand
-system/priv-app/AirReadingGlass
-system/priv-app/SmartEye
+system/preload/Facebook_stub_preload
+system/preload/Instagram_stub_preload
 system/priv-app/AppUpdateCenter
 system/priv-app/AREmoji
 system/priv-app/AREmojiEditor
 system/priv-app/AuthFramework
 system/priv-app/BCService
+system/priv-app/CpAgent
 system/priv-app/DiagMonAgent95
-system/priv-app/DeviceQualityAgent36
 system/priv-app/DigitalKey
 system/priv-app/EnhancedAttestationAgent
 system/priv-app/FBInstaller_NS
 system/priv-app/FBServices
 system/priv-app/FotaAgent
-system/priv-app/GameHome
 system/priv-app/ImsLogger
 system/priv-app/IpsGeofence
-system/priv-app/NetworkDiagnostic
 system/priv-app/OdaService
 system/priv-app/OMCAgent5
 system/priv-app/OneDrive_Samsung_v3
@@ -217,24 +147,14 @@ system/priv-app/PaymentFramework
 system/priv-app/SamsungCarKeyFw
 system/priv-app/SamsungPass
 system/priv-app/SamsungPositioning
-system/priv-app/SamsungBilling
 system/priv-app/SKMSAgent
 system/priv-app/SOAgent76
 system/priv-app/SPPPushClient
 system/priv-app/StickerFaceARAvatar
-system/priv-app/TalkbackSE
+system/priv-app/YourPhone_P1_5
+system/app/BixbyWakeup
 system/priv-app/Bixby
 "
-
-SYSTEM_EXT_DEBLOAT+="
-priv-app/AvatarPicker
-priv-app/GoogleFeedback
-framework/org.carconnectivity.android.digitalkey.rangingintent.jar
-framework/org.carconnectivity.android.digitalkey.secureelement.jar
-"
-
-# Language packs
-SYSTEM_DEBLOAT+="$(find "$WORK_DIR/system" -type d -name "*TTSVoice*" | sed "s|$WORK_DIR/system/||g")"
 
 PRISM_DEBLOAT+="
 app
