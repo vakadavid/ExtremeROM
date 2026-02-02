9626458b633b30ab131fcc1af0399a698cdb1489
vakadavid <134170125+vakadavid@users.noreply.github.com>
2026-02-02 13:53:04 +0100

    Update customize.sh

--- unica/patches/rro/customize.sh
+++ unica/patches/rro/customize.sh
@@ -5,13 +5,14 @@ if [[ -d "$SRC_DIR/target/$TARGET_CODENAME/overlay" ]]; then
 
     LOG "- Applying stock overlay configs"
     rm -rf "$APKTOOL_DIR/product/overlay/framework-res__${SYSTEM_NAME}__auto_generated_rro_product.apk/res"
-    cp -fa \
+    cp -a --preserve=all \
         "$SRC_DIR/target/$TARGET_CODENAME/overlay" \
         "$APKTOOL_DIR/product/overlay/framework-res__${SYSTEM_NAME}__auto_generated_rro_product.apk/res"
 fi
 
 # TODO: Add a proper check if we need to remove this
 DECODE_APK "product" "overlay/SystemUI__${SYSTEM_NAME}__auto_generated_rro_product.apk"
-sed -i -e "/config_enableRoundedCorner/d" "$APKTOOL_DIR/product/overlay/SystemUI__${SYSTEM_NAME}__auto_generated_rro_product.apk/res/values/bools.xml"
+sed -i -e "/config_enableDisplayCutoutProtection/d" -e "/config_enableRoundedCorner/d" "$APKTOOL_DIR/product/overlay/SystemUI__${SYSTEM_NAME}__auto_generated_rro_product.apk/res/values/bools.xml"
+rm "$APKTOOL_DIR/product/overlay/SystemUI__${SYSTEM_NAME}__auto_generated_rro_product.apk/res/values/dimens.xml"
 rm "$APKTOOL_DIR/product/overlay/SystemUI__${SYSTEM_NAME}__auto_generated_rro_product.apk/res/values/public.xml"
 rm "$APKTOOL_DIR/product/overlay/SystemUI__${SYSTEM_NAME}__auto_generated_rro_product.apk/res/drawable/rounded.xml"
