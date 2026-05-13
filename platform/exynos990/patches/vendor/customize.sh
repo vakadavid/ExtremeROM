20af7f4185c1a4cf57d7d960e04ea302b7423b7e
Crecker <104427569+Creeeeger@users.noreply.github.com>
2026-05-13 02:34:03 +0200

    patches: fix fabtic crypto

--- platform/exynos990/patches/vendor/customize.sh
+++ platform/exynos990/patches/vendor/customize.sh
@@ -23,6 +23,27 @@ ADD_TO_WORK_DIR "p3sxxx" "vendor" "lib64"
 ADD_TO_WORK_DIR "p3sxxx" "vendor" "etc/init"
 ADD_TO_WORK_DIR "p3sxxx" "vendor" "etc/vintf"
 
+# Required by newer source framework components using fabric_crypto.
+ADD_TO_WORK_DIR "$SOURCE_FIRMWARE" "vendor" "bin/vendor.samsung.hardware.security.fkeymaster-service"
+ADD_TO_WORK_DIR "$SOURCE_FIRMWARE" "vendor" "etc/init/vendor.samsung.hardware.security.fkeymaster-service.rc"
+ADD_TO_WORK_DIR "$SOURCE_FIRMWARE" "vendor" "etc/vintf/manifest/vendor.samsung.hardware.security.fkeymaster-service.xml"
+ADD_TO_WORK_DIR "$SOURCE_FIRMWARE" "vendor" "lib64/vendor.samsung.hardware.security.fkeymaster-V1-ndk.so"
+
+FKEYMASTER_SEPOLICY_RULES=(
+    "(allow fabriccrypto_${TARGET_VNDK_VERSION}_0 hal_fkey_default (binder (call transfer)))"
+    "(allow hal_fkey_default fabriccrypto_${TARGET_VNDK_VERSION}_0 (binder (transfer)))"
+    "(allow fabriccrypto_${TARGET_VNDK_VERSION}_0 hal_fkey_default (fd (use)))"
+    "(allow fabriccrypto_${TARGET_VNDK_VERSION}_0 hal_fkey_service (service_manager (find)))"
+)
+if grep -q -F "(type hal_fkey_service)" "$WORK_DIR/vendor/etc/selinux/vendor_sepolicy.cil" && \
+    grep -q -F "(type hal_fkey_default)" "$WORK_DIR/vendor/etc/selinux/vendor_sepolicy.cil" && \
+    grep -q -F "(typeattribute fabriccrypto_${TARGET_VNDK_VERSION}_0)" "$WORK_DIR/vendor/etc/selinux/plat_pub_versioned.cil"; then
+    for rule in "${FKEYMASTER_SEPOLICY_RULES[@]}"; do
+        grep -q -F "$rule" "$WORK_DIR/vendor/etc/selinux/vendor_sepolicy.cil" || \
+            echo "$rule" >> "$WORK_DIR/vendor/etc/selinux/vendor_sepolicy.cil"
+    done
+fi
+
 # WPA Supplicant HAL
 if [[ "$TARGET_CODENAME" != "r8s" ]]; then
     ADD_TO_WORK_DIR "p3sxxx" "vendor" "bin/hw/wpa_supplicant"
