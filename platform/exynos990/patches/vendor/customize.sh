bcd56ce939bb3f0a4c41854e35f141cd37ad5809
Creeeeger <104427569+Creeeeger@users.noreply.github.com>
2026-05-20 18:41:07 +0200

    fabric_crypto, fkeym: patches: get them to run.
    fkeymaster: add missing TA
    
    patch: update vendor tee stack from s24fe to allow service access
    
    patch: hal_fkey_default: allow to access tzdev (tz_user_device)
    
    <4>[  430.886827]  [3:tz_iwlog_thread: 1966] SW> IcccOperations: Start communication TA-TA
    <4>[  430.886853]  [3:tz_iwlog_thread: 1966] SW> UREOS VERSION: Samsung Secure OS Release Version 4.1.0.0 built on: 2022-12-30 12:32:22, binary version: e68f621a
    <4>[  430.886861]  [3:tz_iwlog_thread: 1966] SW> ERR: root_task ta_pkg_verify() pid=13: TA_PKG: X509_get_gp_pub_key() failed
    <4>[  430.886869]  [3:tz_iwlog_thread: 1966] SW> SECUREOS VERSION: Samsung Secure OS Release Version 4.1.0.0 built on: 2022-12-30 12:32:22, binary version: e68f621a
    <4>[  430.886877]  [3:tz_iwlog_thread: 1966] SW> ERR: root_task deploy_ta_image() pid=13: Failed to verify TA image (223024 bytes). error: 0xffff0000. UUID = 00000000-0000-0000-0000-4662436b6d52
    <4>[  430.886893]  [4:vendor.samsung.: 6413] TZDEV ERR tzdev_boost_control(151): Trying to disable boost twice, filp=0000000000000000
    <4>[  430.886898]  [3:tz_iwlog_thread: 1966] SW> SECUREOS VERSION: Samsung Secure OS Release Version 4.1.0.0 built on: 2022-12-30 12:32:22, binary version: e68f621a
    <4>[  430.886906]  [3:tz_iwlog_thread: 1966] SW> ERR: root_task get_alive_ta_for_open_session() pid=13: Unable to upload ta image. res: 0xffff0000
    <4>[  430.886926]  [3:tz_iwlog_thread: 1966] SW> SECUREOS VERSION: Samsung Secure OS Release Version 4.1.0.0 built on: 2022-12-30 12:32:22, binary version: e68f621a
    <4>[  430.886933]  [3:tz_iwlog_thread: 1966] SW> ERR: root_task handle_open_session() pid=13: Failed to get alive ta. res: 0xffff0000
    <4>[  430.886940]  [3:tz_iwlog_thread: 1966] SW> SECUREOS VERSION: Samsung Secure OS Release Version 4.1.0.0 built on: 2022-12-30 12:32:22, binary version: e68f621a
    <4>[  430.886947]  [3:tz_iwlog_thread: 1966] SW> ERR: root_task handle_incoming_input() pid=13: Failed to open session. errno: 2
    <4>[  430.886955]  [3:tz_iwlog_thread: 1966] SW> SECUREOS VERSION: Samsung Secure OS Release Version 4.1.0.0 built on: 2022-12-30 12:32:22, binary version: e68f621a
    <4>[  430.886962]  [3:tz_iwlog_thread: 1966] SW> ERR: root_task main() pid=13: Fatal error during process message. sockfd: 28, errno: 2
    <4>[  430.887075]  [4:vendor.samsung.: 6413] TZDEV ERR tzdev_boost_control(151): Trying to disable boost twice, filp=0000000000000000
    unfortunately this is sad since i dont have a TA resigner yet...

--- platform/exynos990/patches/vendor/customize.sh
+++ platform/exynos990/patches/vendor/customize.sh
@@ -28,6 +28,26 @@ ADD_TO_WORK_DIR "$SOURCE_FIRMWARE" "vendor" "bin/vendor.samsung.hardware.securit
 ADD_TO_WORK_DIR "$SOURCE_FIRMWARE" "vendor" "etc/init/vendor.samsung.hardware.security.fkeymaster-service.rc"
 ADD_TO_WORK_DIR "$SOURCE_FIRMWARE" "vendor" "etc/vintf/manifest/vendor.samsung.hardware.security.fkeymaster-service.xml"
 ADD_TO_WORK_DIR "$SOURCE_FIRMWARE" "vendor" "lib64/vendor.samsung.hardware.security.fkeymaster-V1-ndk.so"
+ADD_TO_WORK_DIR "$SOURCE_FIRMWARE" "vendor" "tee/00000000-0000-0000-0000-4662436b6d52" 0 2000 644 "u:object_r:tee_file:s0"
+
+APPEND_SEPOLICY_RULE()
+{
+    local rule="$1"
+
+    grep -q -F "$rule" "$WORK_DIR/vendor/etc/selinux/vendor_sepolicy.cil" || \
+        echo "$rule" >> "$WORK_DIR/vendor/etc/selinux/vendor_sepolicy.cil"
+}
+
+ADD_TYPEATTRIBUTESET_MEMBER()
+{
+    local attribute="$1"
+    local type="$2"
+
+    if grep -q -F "(typeattributeset $attribute " "$WORK_DIR/vendor/etc/selinux/vendor_sepolicy.cil" && \
+        ! grep -q -E "^\\(typeattributeset $attribute .*([[:space:]]|\\()$type([[:space:]]|\\))" "$WORK_DIR/vendor/etc/selinux/vendor_sepolicy.cil"; then
+        sed -i "/^(typeattributeset $attribute /s/))/ $type))/" "$WORK_DIR/vendor/etc/selinux/vendor_sepolicy.cil"
+    fi
+}
 
 FKEYMASTER_SEPOLICY_RULES=(
     "(allow fabriccrypto_${TARGET_VNDK_VERSION}_0 hal_fkey_default (binder (call transfer)))"
@@ -39,15 +59,48 @@ if grep -q -F "(type hal_fkey_service)" "$WORK_DIR/vendor/etc/selinux/vendor_sep
     grep -q -F "(type hal_fkey_default)" "$WORK_DIR/vendor/etc/selinux/vendor_sepolicy.cil" && \
     grep -q -F "(typeattribute fabriccrypto_${TARGET_VNDK_VERSION}_0)" "$WORK_DIR/vendor/etc/selinux/plat_pub_versioned.cil"; then
     for rule in "${FKEYMASTER_SEPOLICY_RULES[@]}"; do
-        grep -q -F "$rule" "$WORK_DIR/vendor/etc/selinux/vendor_sepolicy.cil" || \
-            echo "$rule" >> "$WORK_DIR/vendor/etc/selinux/vendor_sepolicy.cil"
+        APPEND_SEPOLICY_RULE "$rule"
     done
+fi
+if grep -q -F "(typeattribute fabriccrypto_${TARGET_VNDK_VERSION}_0)" "$WORK_DIR/vendor/etc/selinux/plat_pub_versioned.cil" && \
+    grep -q -F "permission_service_${TARGET_VNDK_VERSION}_0" "$WORK_DIR/vendor/etc/selinux/plat_pub_versioned.cil"; then
+    APPEND_SEPOLICY_RULE "(allow fabriccrypto_${TARGET_VNDK_VERSION}_0 permission_service_${TARGET_VNDK_VERSION}_0 (service_manager (find)))"
+fi
+if grep -q -F "(typeattribute fabriccrypto_${TARGET_VNDK_VERSION}_0)" "$WORK_DIR/vendor/etc/selinux/plat_pub_versioned.cil" && \
+    grep -q -F "system_server_${TARGET_VNDK_VERSION}_0" "$WORK_DIR/vendor/etc/selinux/plat_pub_versioned.cil"; then
+    APPEND_SEPOLICY_RULE "(allow fabriccrypto_${TARGET_VNDK_VERSION}_0 system_server_${TARGET_VNDK_VERSION}_0 (binder (call transfer)))"
+    APPEND_SEPOLICY_RULE "(allow system_server_${TARGET_VNDK_VERSION}_0 fabriccrypto_${TARGET_VNDK_VERSION}_0 (binder (transfer)))"
+    APPEND_SEPOLICY_RULE "(allow fabriccrypto_${TARGET_VNDK_VERSION}_0 system_server_${TARGET_VNDK_VERSION}_0 (fd (use)))"
 fi
 FKEYMASTER_TZTS_PROP_RULE="(allow hal_fkey_default vendor_tztsdaemon_prop (file (read getattr map open)))"
 if grep -q -F "(type hal_fkey_default)" "$WORK_DIR/vendor/etc/selinux/vendor_sepolicy.cil" && \
     grep -q -F "(type vendor_tztsdaemon_prop)" "$WORK_DIR/vendor/etc/selinux/vendor_sepolicy.cil"; then
-    grep -q -F "$FKEYMASTER_TZTS_PROP_RULE" "$WORK_DIR/vendor/etc/selinux/vendor_sepolicy.cil" || \
-        echo "$FKEYMASTER_TZTS_PROP_RULE" >> "$WORK_DIR/vendor/etc/selinux/vendor_sepolicy.cil"
+    APPEND_SEPOLICY_RULE "$FKEYMASTER_TZTS_PROP_RULE"
+fi
+FKEYMASTER_TZDEV_RULE="(allow hal_fkey_default tz_user_device (chr_file (ioctl read write getattr lock append map open watch watch_reads)))"
+if grep -q -F "(type hal_fkey_default)" "$WORK_DIR/vendor/etc/selinux/vendor_sepolicy.cil" && \
+    grep -q -F "(type tz_user_device)" "$WORK_DIR/vendor/etc/selinux/vendor_sepolicy.cil"; then
+    APPEND_SEPOLICY_RULE "$FKEYMASTER_TZDEV_RULE"
+fi
+if ! grep -q -F "(type vendor_teegris_prop)" "$WORK_DIR/vendor/etc/selinux/vendor_sepolicy.cil"; then
+    echo "(type vendor_teegris_prop)" >> "$WORK_DIR/vendor/etc/selinux/vendor_sepolicy.cil"
+    echo "(roletype object_r vendor_teegris_prop)" >> "$WORK_DIR/vendor/etc/selinux/vendor_sepolicy.cil"
+fi
+if grep -q -F "(type vendor_teegris_prop)" "$WORK_DIR/vendor/etc/selinux/vendor_sepolicy.cil"; then
+    ADD_TYPEATTRIBUTESET_MEMBER "property_type" "vendor_teegris_prop"
+    ADD_TYPEATTRIBUTESET_MEMBER "vendor_property_type" "vendor_teegris_prop"
+    ADD_TYPEATTRIBUTESET_MEMBER "vendor_public_property_type" "vendor_teegris_prop"
+    grep -q -F "vendor.TEEGRIS." "$WORK_DIR/vendor/etc/selinux/vendor_property_contexts" || \
+        echo "vendor.TEEGRIS.                             u:object_r:vendor_teegris_prop:s0" >> "$WORK_DIR/vendor/etc/selinux/vendor_property_contexts"
+    grep -q -F "vendor.teegris." "$WORK_DIR/vendor/etc/selinux/vendor_property_contexts" || \
+        echo "vendor.teegris.                             u:object_r:vendor_teegris_prop:s0" >> "$WORK_DIR/vendor/etc/selinux/vendor_property_contexts"
+    if grep -q -F "(type hal_fkey_default)" "$WORK_DIR/vendor/etc/selinux/vendor_sepolicy.cil"; then
+        APPEND_SEPOLICY_RULE "(allow hal_fkey_default vendor_teegris_prop (file (read getattr map open)))"
+    fi
+    if grep -q -F "vendor_init_${TARGET_VNDK_VERSION}_0" "$WORK_DIR/vendor/etc/selinux/plat_pub_versioned.cil"; then
+        APPEND_SEPOLICY_RULE "(allow vendor_init_${TARGET_VNDK_VERSION}_0 vendor_teegris_prop (property_service (set)))"
+        APPEND_SEPOLICY_RULE "(allow vendor_init_${TARGET_VNDK_VERSION}_0 vendor_teegris_prop (file (read getattr map open)))"
+    fi
 fi
 
 # WPA Supplicant HAL
