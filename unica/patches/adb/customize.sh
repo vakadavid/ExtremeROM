17b893f3fd644f487d4e4b92a9300dd2446ff892
Creeeeger <104427569+Creeeeger@users.noreply.github.com>
2026-05-20 12:52:03 +0200

    patches: adb: enable secure adb

--- unica/patches/adb/customize.sh
+++ unica/patches/adb/customize.sh
@@ -12,11 +12,6 @@ SET_PROP_IF_DIFF "system_dlkm" "persist.sys.usb.config" "$(GET_PROP "system_dlkm
 SET_PROP_IF_DIFF "vendor" "persist.sys.usb.config" "$(GET_PROP "vendor" "persist.sys.usb.config"),adb"
 SET_PROP_IF_DIFF "vendor_dlkm" "persist.sys.usb.config" "$(GET_PROP "vendor_dlkm" "persist.sys.usb.config"),adb"
 
-# Disable adb authentication
-# https://android.googlesource.com/platform/packages/modules/adb/+/refs/tags/android-15.0.0_r1/daemon/main.cpp#213
-SET_PROP_IF_DIFF "system" "ro.adb.secure" "0"
-SET_PROP_IF_DIFF "vendor" "ro.adb.secure" "0"
-
 # Do not filter out Samsung processes in logs
 SET_PROP_IF_DIFF "system" "persist.log.semlevel" "0xFFFFFFFF"
 
